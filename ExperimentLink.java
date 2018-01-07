7ebf428cd5ee20b2bb3208503406101c05452cfe
tatpiya1 <31882814+tatpiya1@users.noreply.github.com>
2018-01-07 15:59:32 +0700

    Add files via upload

---
+++ ExperimentLink.java
@@ -0,0 +1,16 @@
+package LinkList;
+
+public class Link {
+	  public long data;
+	  public Link next;
+
+	  public Link(long val) {
+	    data = val;
+	    next = null;
+	  }
+
+	  public void displayLink() {
+	    System.out.print(data + " ");
+	  }
+
+	}  // end class
