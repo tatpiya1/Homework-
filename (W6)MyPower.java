289298b92aef55afcf456ef73f0201bc1e332e4a
tatpiya1 <31882814+tatpiya1@users.noreply.github.com>
2018-01-07 15:58:38 +0700

    Add files via upload
    Tatpiya_Jirasakrojn 5907101005

---
+++ (W6)MyPower.java
@@ -0,0 +1,27 @@
+/**
+@Tatpiya_Jirasakrojn 5907101005
+ * */
+package th.ac.utcc.cpe.sirawit;
+
+public class MyPower {
+	public static void main(String[] args){
+		long x =  3, y= 18;
+		System.out.println("Pow("+x+","+y+")= "+myPower(x,y));
+		}
+		public static long myPower(long x , long y){
+		System.out.println("x="+x+",y="+y);
+		long ans = x;
+		if (y !=1 ){
+		 if (y%2 != 0){
+			 ans= x*myPower(x*x,y/2);
+		 System.out.println("Returning "+ans+",X="+x+",Y="+y+" // Y is odd");}
+		 else{ ans= myPower(x*x,y/2); // y is odd
+		System.out.println("Returning "+ans+",X="+x+",Y="+y+ " // Y is even");}
+		return ans ;
+		}
+		else {
+		System.out.println("Returning "+ans+",X="+x+",Y="+y);
+		return ans;
+		}
+		}
+}
